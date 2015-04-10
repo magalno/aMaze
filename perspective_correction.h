cd8231a6a0715697555599d8d53e06d4ae154e04
Jan Ove Saltvedt <jan.ove@saltvedt.no>
2015-04-10 12:41:40 +0200

    Template for perspective correction class

---
+++ perspective_correction.h
@@ -0,0 +1,16 @@
+#ifndef PERSPECTIVE_CORRECTION_H
+#define PERSPECTIVE_CORRECTION_H
+
+#include <opencv2/imgproc/imgproc.hpp>
+
+class PerspectiveCorrection
+{
+public:
+    PerspectiveCorrection(cv::Mat &src);
+    ~PerspectiveCorrection();
+
+    bool process();
+    cv::Mat getResult();
+}
+
+#endif
